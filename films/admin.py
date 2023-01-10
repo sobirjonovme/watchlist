08e799bd498f66884bd15c2779fc5f6b19287e8d
sobirjonovme <huntsman6004@gmail.com>
2023-01-10 17:01:41 +0500

    StreamPlatform, Film, Review models created in films_app

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import StreamPlatform, Film, Review
+
 # Register your models here.
+admin.site.register(StreamPlatform)
+admin.site.register(Film)
+admin.site.register(Review)
--- films/models.py
+++ films/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+from users.models import CustomUser
+
 
 # Create your models here.
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=30)
+    about = models.CharField(max_length=150)
+    website = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Film(models.Model):
+    title = models.CharField(max_length=60)
+    storyline = models.TextField()
+    active = models.BooleanField(default=True)
+    platform = models.ForeignKey(
+        StreamPlatform,
+        on_delete=models.CASCADE,
+        related_name='platform_films'
+    )
+    # avg_rating = models.FloatField(default=0)
+    # number_rating = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    review_author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='user_reviews'
+    )
+    rating = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    review_text = models.CharField(max_length=250)
+    film = models.ForeignKey(
+        Film,
+        on_delete=models.CASCADE,
+        related_name='film_reviews'
+    )
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.rating} | {self.film.title} | {self.review_author}"
