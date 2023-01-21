8940bc0da776bc27034efb51ac94194800717ed9
sobirjonovme <huntsman6004@gmail.com>
2023-01-21 12:18:50 +0500

    added: logout view and url

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import RegistrationAPIView
+from users.api.views import RegistrationAPIView, LogOutAPIView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', RegistrationAPIView.as_view(), name='register'),
+    path('logout/', LogOutAPIView.as_view(), name='logout'),
 
 ]
--- users/api/views.py
+++ users/api/views.py
@@ -1,4 +1,8 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import RegistrationSerializer
 from users.models import CustomUser
@@ -7,3 +11,16 @@ from users.models import CustomUser
 class RegistrationAPIView(CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegistrationSerializer
+
+
+class LogOutAPIView(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request):
+        request.user.auth_token.delete()
+
+        return Response(
+            data={'message': 'You logged out successfully!'},
+            status=status.HTTP_200_OK
+        )
+
